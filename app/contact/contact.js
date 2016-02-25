9766b654cb4cbed59f47e4a3d51d60acc939639f
Kevin Coffey <kevdougful@gmail.com>
2016-02-24 22:23:58 -0600

    added contact view and controller

---
+++ app/contact/contact.js
@@ -0,0 +1,14 @@
+'use strict';
+
+angular.module('kcoffey-app.contact', ['ngRoute'])
+
+.config(['$routeProvider', function($routeProvider) {
+	$routeProvider.when('/contact', {
+		templateUrl: 'contact.html',
+		controller: 'ContactCtrl'
+	});
+}])
+
+.controller('ContactCtrl', ['$scope', function($scope) {
+	
+}]);
